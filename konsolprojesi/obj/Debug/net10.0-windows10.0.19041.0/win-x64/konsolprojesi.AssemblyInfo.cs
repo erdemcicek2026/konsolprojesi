8dcca41c65cde6b8cbd0b6a81e1c361c054e20bf
erdemcicek2026 <erdemcicek2026@gmail.com>
2026-04-20 09:13:33 +0300

    deneme2

--- konsolprojesi/obj/Debug/net10.0-windows10.0.19041.0/win-x64/konsolprojesi.AssemblyInfo.cs
+++ konsolprojesi/obj/Debug/net10.0-windows10.0.19041.0/win-x64/konsolprojesi.AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("konsolprojesi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+6b55beb2f061dd41f51a22ed7ee385617631e575")]
 [assembly: System.Reflection.AssemblyProductAttribute("konsolprojesi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("konsolprojesi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
